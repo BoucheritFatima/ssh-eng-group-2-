45510a0643d090d7052581588e1a99119f17fc45
boucherit fatima <fatimaboucherit11@gmail.com>
2026-05-04 11:15:42 +0100

    Add English test exercises to readme
    Added exercises for English language practice including Present Simple, types of sentences, and formal vs informal writing.

--- readme.m
+++ readme.m
@@ -1,1 +1,33 @@
+  Nour Elbachir University Center of El Bayadh  
+Department: French Studies
+Module: English                                                            Level: 1st  /2nd Years
+English Test
+ 
+Exercise 1: Present Simple
+1. My sister ______ (be) very polite.
+2. We ______ (have) English twice a week.
+3. I usually ______ (do) my revision at night.
+4. He ______ (not / like) speaking in public.
+                B. Underline the correct auxiliary:
+1. (Do / Does) she like English?
+2. (Is / Are) they ready for the exam?
+3. (Do / Does) you have a dictionary?
+Exercise 2: Types of Sentences
+ A. Identify the type of each sentence: (declarative- interrogative-imperative-exclamatory)
+1. We are late.                                                ………………………………………
+2. Do you understand the lesson?                 ……………………………………….
+3. Close the door, please.                              …………………………………………
+4.  what an amazing result!                           ………………………………………..
+
+Exercise 3: Formal vs Informal
+                           A. Write Formal and Informal for each sentence :
+1. Hi! How are you doing?                    ……………………
+2. Dear Sir, I am writing to inform you   ………………………
+3. What’s up?...............................
+4. Good morning, how are you? ………………………..
+5. I would like to invite you to a meeting. …………………………..
+Exercise 4: Writing
+Write a short  paragraph  introducing yourself  , your daily routine and your opinion about English.
+…………………………………………………………………………………………………………………………………………………………………………………………………………………….……………………………………………………………………………………………………………………………………………………………………..
+
 
